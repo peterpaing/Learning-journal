37813fa514fae55c27ff53d04683afaba30b275e
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-15 00:57:17 +0630

    add data to data.js and connect data.js to index.js

---
+++ data.js
@@ -0,0 +1,31 @@
+
+const mainPost=[
+    {image:"main-photo.png" ,
+    date:"JULY 23, 2022",
+    title:"My new journey as a bootcamp student.",
+    text:"After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
+}
+]
+
+const recentPosts =[
+
+    {image:"/images/recent-photo-1.png" ,
+     date :"JULY 23, 2022",
+     title:"Blog one"  ,
+     text : "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."   
+    },
+
+    {image:"/images/recent-photo-2.png",
+     date :"JULY 23, 2022",
+     title:"Blog two",
+     text :"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."    
+    },
+
+    {image:"/images/recent-photo-3.png",
+     date : "JULY 23, 2022",
+     title:"Blog three",
+     text :"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."    
+    }
+]
+
+export {mainPost , recentPosts}
---
+++ index.js
@@ -0,0 +1,1 @@
+import {mainPost , recentPosts} from './data.js'
