4430fdfee37454bc38f0b6c30054b791fd64d1d0
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-17 21:55:53 +0630

    update content in About Me section for clarity and detail

--- index.js
+++ index.js
@@ -50,10 +50,11 @@ function viewMainPost() {
 
             <div class="text-more">
                 <p class="title">How I stay committed to learning</p>
-                <p>I like to think of myself as a lifelong learner...</p>
-
+                <p>I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.</p>
+                <p>While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.</p>
                 <p class="title">How I got started</p>
-                <p>I started simple and gradually grew my learning journal site...</p>
+                <p>I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.</p>
+                <p>That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.</p>
             </div>
 
             <h3 class="recent-post-title">Recent posts</h3>
@@ -77,10 +78,11 @@ function renderAbout() {
 
         <div class="text-more">
             <p class="title">How I stay committed to learning</p>
-            <p>I like to think of myself as a lifelong learner...</p>
-
+            <p>I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.</p>
+            <p>While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.</p>
             <p class="title">How I got started</p>
-            <p>I started simple and gradually grew my learning journal site...</p>
+            <p>I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.</p>
+            <p>That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.</p>
         </div>
 
         <h3 class="recent-post-title">Recent posts</h3>
